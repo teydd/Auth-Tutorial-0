3819cc1b7fcf650a9eae0f59a0910de43b59dae4
teydd <chemosted@gmail.com>
2025-10-01 22:18:57 +0300

    auth model

---
+++ server/models/authModel.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose")
+
+const UserSchema = new mongoose.Schema({
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    name:{
+        type:String,
+        required:true
+    },
+    isVerified:{
+        type:Boolean,
+        default:false
+    },
+    role:{
+        type:String,
+        enum:['customer','admin'],
+        default:'customer'
+    },
+    verificationToken:String,
+    verificationTokenExpiresAt:String,
+    resetPasswordToken:String,
+    resetPasswordTokenExpiresAt:String
+},{timestamps:true})
+
+const User = mongoose.model("user",UserSchema)
+
+module.exports = User
