72c409a3dafaae20afc74b320b3e21064ee9c30e
teydd <chemosted@gmail.com>
2025-10-03 17:48:50 +0300

    dash jsx

---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Dashboard() {
+  return (
+    <>
+    <p className="text-center lead">Welcome to the Dashboard</p>
+    <div className="container col-sm-6 col-lg-6 dash">
+        <p className="text-center  p-5">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex sit inventore maxime, molestiae accusamus atque necessitatibus libero iure numquam, voluptas tempora? Amet officia dignissimos cumque molestiae culpa, neque eius incidunt.
+        </p>
+    </div>
+    </>
+  )
+}
