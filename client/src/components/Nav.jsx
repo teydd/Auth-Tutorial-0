364fd96f5ad844ecc5a9c47ebbba290d3751dec8
teydd <chemosted@gmail.com>
2025-10-05 22:28:01 +0300

    prettier

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,28 +1,48 @@
-import React from 'react'
-import { Link, Links } from 'react-router-dom'
+import React from "react";
+import { Link, Links } from "react-router-dom";
 
 export default function Nav() {
   return (
     <>
-    <nav className="navbar navbar-expand-lg shadow-lg">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="/">Auth Tutorial</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav w-100 justify-content-center">
-        <li className="nav-item">
-            <Link className="nav-link active" aria-current="page" to={"/"}>Home</Link>
-        </li>
-        <li className="nav-item">
-            <Link className="nav-link active" aria-current="page" to={"/dashboard"}>Dashboard</Link>
-        </li>
-      </ul>
-      <Link to={"/signin"} className='btn btn-danger'>Signin</Link>
-    </div>
-  </div>
-</nav>
+      <nav className="navbar navbar-expand-lg shadow-lg">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">
+            Auth Tutorial
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav w-100 justify-content-center">
+              <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" to={"/"}>
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className="nav-link active"
+                  aria-current="page"
+                  to={"/dashboard"}
+                >
+                  Dashboard
+                </Link>
+              </li>
+            </ul>
+            <Link to={"/signin"} className="btn btn-danger">
+              Signin
+            </Link>
+          </div>
+        </div>
+      </nav>
     </>
-  )
+  );
 }
