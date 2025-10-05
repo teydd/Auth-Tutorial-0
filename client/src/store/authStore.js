f34c1b5b95b0cdb28a2c539625d6e8fb0d965250
teydd <chemosted@gmail.com>
2025-10-05 22:28:24 +0300

    prettier

--- client/src/store/authStore.js
+++ client/src/store/authStore.js
@@ -1,31 +1,31 @@
-import {create} from "zustand"
+import { create } from "zustand"
 import axios from "axios"
 
-const URL ="http://localhost:2000/auth"
+const URL = "http://localhost:2000/auth"
 axios.defaults.withCredentials = true
 
-export const useAuthStore = create((set)=>({
-    user:null,
-    isLoading:false,
-    isAuthenticated:false,
-    error:null,
-    isCheckingAuth:true,
+export const useAuthStore = create((set) => ({
+    user: null,
+    isLoading: false,
+    isAuthenticated: false,
+    error: null,
+    isCheckingAuth: true,
 
-    signup:async(email,password,name,tel)=>{
-        set({isLoading:true,error:null})
+    signup: async (email, password, name, tel) => {
+        set({ isLoading: true, error: null })
         try {
-            const response = await axios.post(`${URL}/signup`,{email,password,name,tel})
+            const response = await axios.post(`${URL}/signup`, { email, password, name, tel })
             set({
-                user:response.data.user,
-                isAuthenticated:true,
-                isLoading:false
+                user: response.data.user,
+                isAuthenticated: true,
+                isLoading: false
             })
         } catch (error) {
-            set({error:error.response.data.message || "Error signing up",isLoading:false})     
-            throw error       
+            set({ error: error.response.data.message || "Error signing up", isLoading: false })
+            throw error
         }
     },
-    verify:async()=>{
+    verify: async () => {
 
     },
 }))
