1462183f3b75081a11e98f706e52a165d1dc11cb
teydd <chemosted@gmail.com>
2025-10-05 22:27:38 +0300

    prettier

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,29 @@
-import { useState } from 'react'
+import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import{BrowserRouter as Router,Routes,Route} from "react-router-dom"
-import Nav from './components/Nav'
-import Home from './pages/Home'
-import Dashboard from './pages/Dashboard';
-import Signup from './pages/Signup';
-import Signin from './pages/Signin';
-import Verify from './pages/Verify';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Nav from "./components/Nav";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
+import Verify from "./pages/Verify";
 
 function App() {
-  
-
   return (
     <>
       <Router>
         <Nav></Nav>
         <Routes>
-          <Route path='/' element={<Home></Home>}></Route>
-          <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
-          <Route path='/signup' element={<Signup></Signup>}></Route>
-          <Route path='/signin' element={<Signin></Signin>}></Route>
-          <Route path='/verify' element={<Verify></Verify>}></Route>
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
+          <Route path="/signup" element={<Signup></Signup>}></Route>
+          <Route path="/signin" element={<Signin></Signin>}></Route>
+          <Route path="/verify" element={<Verify></Verify>}></Route>
         </Routes>
       </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
