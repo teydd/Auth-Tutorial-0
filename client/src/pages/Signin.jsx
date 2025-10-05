b56b3c82a57d47db79a68475b4dc64a136f461ef
teydd <chemosted@gmail.com>
2025-10-05 22:28:15 +0300

    prettier

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -3,11 +3,12 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Signin() {
-
   return (
     <div className="align-items-center justify-content-center d-flex mt-5 ">
-      <form  className="form p-5 rounded-4 dash">
-        <Link className='text-decoration-none text-black' to={"/"}><h3 className='text-center'>Auth Tutorial</h3></Link>
+      <form className="form p-5 rounded-4 dash">
+        <Link className="text-decoration-none text-black" to={"/"}>
+          <h3 className="text-center">Auth Tutorial</h3>
+        </Link>
         <p className="text-center">Sign up and have an account</p>
         <hr />
         <input
@@ -32,7 +33,10 @@ export default function Signin() {
         </button>
         <hr />
         <p>
-          Already have an account? <Link className="text-black text-decoration-none" to="/signup">Signup</Link>
+          Already have an account?{" "}
+          <Link className="text-black text-decoration-none" to="/signup">
+            Signup
+          </Link>
         </p>
       </form>
     </div>
