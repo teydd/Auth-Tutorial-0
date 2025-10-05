b484d869599fd3ab94b71b38f39c3c9d9a842329
teydd <chemosted@gmail.com>
2025-10-05 22:28:19 +0300

    prettier

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,38 +1,40 @@
-import React, { useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom';
-import { useAuthStore } from "../store/authStore"
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuthStore } from "../store/authStore";
 
 export default function Signup() {
-  const [form,setForm] = useState({
-    email:"",
-    password:"",
-    name:"",
-    tel:"",
-  })
- const navigate = useNavigate()
-     const {signup,error,isLoading} = useAuthStore()
-  const handleSubmit = async (e)=>{
-    const {email,password,name,tel} = form
-    e.preventDefault()
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+    name: "",
+    tel: "",
+  });
+  const navigate = useNavigate();
+  const { signup, error, isLoading } = useAuthStore();
+  const handleSubmit = async (e) => {
+    const { email, password, name, tel } = form;
+    e.preventDefault();
     try {
-      await signup(email,password,name,tel)
-      navigate("/verify")      
+      await signup(email, password, name, tel);
+      navigate("/verify");
     } catch (error) {
-      console.log("Error handlesubmit",error)      
+      console.log("Error handlesubmit", error);
     }
-  }
-  const handleChange = (e)=>{
-    const {name,value} = e.target
-    setForm((prev)=>({
+  };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm((prev) => ({
       ...prev,
-      [name]:value
-    }))
-  }
+      [name]: value,
+    }));
+  };
 
   return (
     <div className="align-items-center justify-content-center d-flex mt-5 ">
-      <form onSubmit={handleSubmit}  className="dash p-5 rounded-4">
-        <Link className='text-decoration-none text-black' to={"/"}><h3 className='text-center'>Auth Tutorial</h3></Link>
+      <form onSubmit={handleSubmit} className="dash p-5 rounded-4">
+        <Link className="text-decoration-none text-black" to={"/"}>
+          <h3 className="text-center">Auth Tutorial</h3>
+        </Link>
         <p className="text-center">Sign up and have an account</p>
         <hr />
         <input
@@ -72,20 +74,21 @@ export default function Signup() {
           value={form.password}
         />
         <br />
-        {error && <p className='text-black'>{error}</p>}
+        {error && <p className="text-black">{error}</p>}
         <button
           type="submit"
           className="btn btn-lg btn-outline-success active w-100"
         >
-          {isLoading ? "Signing up ": "Submit"}
+          {isLoading ? "Signing up " : "Submit"}
         </button>
         <hr />
         <p>
-          Already have an account? <Link className="text-black text-decoration-none" to="/signin">Signin</Link>
+          Already have an account?{" "}
+          <Link className="text-black text-decoration-none" to="/signin">
+            Signin
+          </Link>
         </p>
       </form>
     </div>
   );
 }
-
- 
