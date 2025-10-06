97694480c5cf6b5a282a8c216dc6b380ca0129ae
teydd <chemosted@gmail.com>
2025-10-06 23:15:14 +0300

    verify update

--- client/src/pages/Verify.jsx
+++ client/src/pages/Verify.jsx
@@ -1,16 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 export default function Verify() {
+  const [form,setForm] = useState({code:""})
+  
+  const handleSubmit = (e)=>{
+    e.preventDefault()
+  }
+  const handleChange = (e)=>{
+   
+  }
   return (
     <>
      <div className="align-items-center justify-content-center d-flex mt-5 ">
-      <form  className="form p-5 rounded-4 dash">
+      <form onSubmit={handleSubmit} className="form p-5 rounded-4 dash">
         <Link className='text-decoration-none text-black' to={"/"}><h3 className='text-center'>Verify account</h3></Link>
         <hr />
         <input
           className="form-control text-center fs-5"
+          name='code'
           maxLength={6}
+          onChange={handleChange}
+          value={form.code}
+          autoFocus
         />
         <br />
       </form>
