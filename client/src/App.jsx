aa1215e4a7312d4af8f4bf08ff525886d909d67f
teydd <chemosted@gmail.com>
2025-10-06 23:13:52 +0300

    reset and forget pass routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,8 @@ import Dashboard from "./pages/Dashboard";
 import Signup from "./pages/Signup";
 import Signin from "./pages/Signin";
 import Verify from "./pages/Verify";
+import ForgetPassword from "./pages/ForgetPassword";
+import ResetPassword from "./pages/ResetPassword";
 
 function App() {
   return (
@@ -20,6 +22,8 @@ function App() {
           <Route path="/signup" element={<Signup></Signup>}></Route>
           <Route path="/signin" element={<Signin></Signin>}></Route>
           <Route path="/verify" element={<Verify></Verify>}></Route>
+          <Route path="/forgot-password" element={<ForgetPassword></ForgetPassword>}></Route>
+          <Route path="/reset-password" element={<ResetPassword></ResetPassword>}></Route>
         </Routes>
       </Router>
     </>
