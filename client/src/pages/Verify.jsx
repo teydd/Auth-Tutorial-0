808795030a699af148ab699b272f068085584d1c
teydd <chemosted@gmail.com>
2025-10-07 21:04:53 +0300

    verify functionality

--- client/src/pages/Verify.jsx
+++ client/src/pages/Verify.jsx
@@ -1,14 +1,30 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { useAuthStore } from '../store/authStore'
+import toast from 'react-hot-toast'
 
 export default function Verify() {
-  const [form,setForm] = useState({code:""})
-  
-  const handleSubmit = (e)=>{
-    e.preventDefault()
-  }
+  const [code,setCode] = useState("")
+  const {verify,error} = useAuthStore()
+  useEffect(()=>{
+    if(code.length ===6){
+      handleSubmit()
+    }
+  },[code])
+
   const handleChange = (e)=>{
-   
+    setCode(e.target.value.replace(/\D/g,"").slice(0,6))
+  }
+  const navigate = useNavigate()
+  
+  const handleSubmit = async (e)=>{
+    if(e) e.preventDefault()
+      if(code.length === 6){
+        await verify(code)
+        navigate("/dashboard")
+        toast.success("Verified successfully")
+      }
+      
   }
   return (
     <>
@@ -18,13 +34,13 @@ export default function Verify() {
         <hr />
         <input
           className="form-control text-center fs-5"
-          name='code'
+          type='text'
           maxLength={6}
           onChange={handleChange}
-          value={form.code}
+          value={code}
           autoFocus
         />
-        <br />
+        <p className='text-center text-danger'>{error}</p>
       </form>
     </div>
     </>
