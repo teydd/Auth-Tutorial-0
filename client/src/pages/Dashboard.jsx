f7e4efb0f84691f7a79972cf63de3fd6d64dc651
teydd <chemosted@gmail.com>
2025-10-09 13:38:54 +0300

    dashboard update

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,16 +1,37 @@
 import React from "react";
-
+import ProfileCard from "../bits/ProfileCard";
+import teddy from "../assets/teddy.jpg"
+import { useAuthStore } from "../store/authStore";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
 export default function Dashboard() {
+  const {user,logout} = useAuthStore()
+  const navigate = useNavigate()
+
+  const handleLogOut = async()=>{
+    await logout()
+    navigate("/signin")
+    toast.success("Logout successful")
+
+  }
   return (
     <>
-      <p className="text-center lead">Welcome to the Dashboard</p>
-      <div className="container col-sm-6 col-lg-6 dash">
-        <p className="text-center  p-5">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex sit
-          inventore maxime, molestiae accusamus atque necessitatibus libero iure
-          numquam, voluptas tempora? Amet officia dignissimos cumque molestiae
-          culpa, neque eius incidunt.
-        </p>
+      <p className="text-center lead py-2">Welcome {user?.name}</p>
+      <button onClick={handleLogOut} className="float-end btn btn-danger">Logout</button>
+      <br />
+      <div className="align-items-center justify-content-center d-flex" style={{height:"70vh"}}>
+        <ProfileCard
+  name="Teddy Chemos"
+  title="Software Engineer"
+  handle="Teddy"
+  status="Online"
+  contactText="Contact Me"
+  avatarUrl={teddy}
+  showUserInfo={true}
+  enableTilt={true}
+  enableMobileTilt={true}
+  onContactClick={() => console.log('Contact clicked')}
+/>
       </div>
     </>
   );
