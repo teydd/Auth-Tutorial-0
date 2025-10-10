cbf88c573de932e8c38c25574a6d8f0458ebcd86
teydd <chemosted@gmail.com>
2025-10-10 20:23:49 +0300

    update

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({origin:"http://localhost:5173",credentials:true}))
+app.use(cors({origin:process.env.CLIENT_ORIGIN,credentials:true}))
 
 app.use(helmet())
 app.use(ratelimit({
