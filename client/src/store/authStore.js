34151962e967caaf4645b645319406d33a76a00b
teydd <chemosted@gmail.com>
2025-10-10 22:01:54 +0300

    check auth

--- client/src/store/authStore.js
+++ client/src/store/authStore.js
@@ -38,7 +38,7 @@ export const useAuthStore = create((set) => ({
     checkAuth:async()=>{
         set({isCheckingAuth:true,error:null})
         try {
-            const response = await axios.get(`${SERVER_URL}/check-auth`)
+            const response = await axios.get(`${SERVER_URL}/check-auth`,{withCredentials:true})
             set({user:response.data.user,isAuthenticated:true,isCheckingAuth:false})
         } catch (error) {
             set({error:null,isCheckingAuth:false,isAuthenticated:false})            
