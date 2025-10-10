88d2227bf7d3efb09a6e0498b9f1a893be9c5321
teydd <chemosted@gmail.com>
2025-10-10 19:17:10 +0300

    helmet and rate limit integration

--- server/index.js
+++ server/index.js
@@ -5,6 +5,9 @@ const cors = require("cors")
 require("dotenv").config()
 const auth = require("./routes/authRoute")
 
+const helmet = require("helmet")
+const ratelimit = require("express-rate-limit")
+
 PORT = process.env.PORT
 
 const app = express()
@@ -12,12 +15,19 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({origin:"http://localhost:5173",credentials:true}))
+
+app.use(helmet())
+app.use(ratelimit({
+    widowMS:10*60*1000,
+    max:50
+}))
+
 app.use("/auth",auth)
 
 mongoose.connect(process.env.Test).then(()=>{
-    console.log("Db connected")
-    app.listen(PORT, ()=>{
-        console.log("Server running on port ", PORT)
+    console.log("DB connected succesfully")
+    app.listen(PORT,()=>{
+        console.log("Server running in port", PORT)
     })
 }).catch(()=>{
     console.log("Error connecting to DB")
