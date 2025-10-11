24297d612464a5f2d57bb2182beb0ebadda41983
teydd <chemosted@gmail.com>
2025-10-11 23:34:23 +0300

    Prettier

--- server/middleware/verifyToken.js
+++ server/middleware/verifyToken.js
@@ -1,28 +1,31 @@
-const jwt = require ("jsonwebtoken")
-require("dotenv").config()
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
+const verifyToken = (req, res, next) => {
+  const token = req.cookies.token;
+  if (!token) {
+    return res
+      .status(401)
+      .json({ message: "Unauthorised - Invalid or Expired token" });
+  }
 
-const verifyToken = (req,res,next)=>{
-    const token = req.cookies.token
-    if(!token){
-        return res.status(401).json({message:"Unauthorised - Invalid or Expired token"})
-    }
+  try {
+    const { userId, role } = jwt.verify(token, process.env.JWT);
+    req.userId = userId;
+    req.role = role;
+    next();
+  } catch (error) {
+    console.log("Error verify token middleware");
+    res.status(400).json({ message: error.message });
+  }
+};
 
-    try {
-        const {userId, role} = jwt.verify(token,process.env.JWT)
-        req.userId = userId
-        req.role = role
-        next()
-    } catch (error) {
-        console.log("Error verify token middleware")
-        res.status(400).json({message:error.message})
-    }
-}
-
-const isAdmin = (req,res,next)=>{
-    if(!req.role || req.role != "admin"){
-        return res.status(401).json({message:"Unauthorised access - Admin Only"})
-    }
-    next()
-}
-module.exports = {verifyToken,isAdmin}
+const isAdmin = (req, res, next) => {
+  if (!req.role || req.role != "admin") {
+    return res
+      .status(401)
+      .json({ message: "Unauthorised access - Admin Only" });
+  }
+  next();
+};
+module.exports = { verifyToken, isAdmin };
