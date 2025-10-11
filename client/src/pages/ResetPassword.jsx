7e6bd17e6fc351c56443c9ab68d6ec6b2bbf735b
teydd <chemosted@gmail.com>
2025-10-11 23:33:40 +0300

    Prettier

--- client/src/pages/ResetPassword.jsx
+++ client/src/pages/ResetPassword.jsx
@@ -1,41 +1,41 @@
-import React, { useState } from 'react'
-import { Link, useNavigate, useParams } from 'react-router-dom'
-import { useAuthStore } from '../store/authStore'
-import toast from 'react-hot-toast'
+import React, { useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { useAuthStore } from "../store/authStore";
+import toast from "react-hot-toast";
 
 export default function ResetPassword() {
-    const [form,setForm] = useState({
-        password:"",
-        newpassword:""
-    })
-    const {resetPassword,isLoading,error}=useAuthStore()
-    const {token} =useParams()
-    const navigate =useNavigate()
+  const [form, setForm] = useState({
+    password: "",
+    newpassword: "",
+  });
+  const { resetPassword, isLoading, error } = useAuthStore();
+  const { token } = useParams();
+  const navigate = useNavigate();
 
-    const handleSubmit = async(e)=>{
-      const {password} = form
-        e.preventDefault()
-        if(form.password !== form.newpassword){
-          toast.error("Passwords do not match")
-          return
-        }
-        try {
-           await resetPassword(token,password)
-        toast.success("Password reset successfully")
-        navigate("/signin")
-        } catch (error) {
-          toast.error("Error resetting password")          
-        }
+  const handleSubmit = async (e) => {
+    const { password } = form;
+    e.preventDefault();
+    if (form.password !== form.newpassword) {
+      toast.error("Passwords do not match");
+      return;
     }
-    const handleChange = (e)=>{
-        const {name,value} = e.target
-        setForm((prev)=>({
-            ...prev,
-            [name]:value
-        }))
+    try {
+      await resetPassword(token, password);
+      toast.success("Password reset successfully");
+      navigate("/signin");
+    } catch (error) {
+      toast.error("Error resetting password");
     }
+  };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
   return (
-   <div className="align-items-center justify-content-center d-flex mt-5 ">
+    <div className="align-items-center justify-content-center d-flex mt-5 ">
       <form onSubmit={handleSubmit} className="form p-5 rounded-4 dash">
         <Link className="text-decoration-none text-black" to={"/"}>
           <h3 className="text-center">Auth Tutorial</h3>
@@ -51,7 +51,6 @@ export default function ResetPassword() {
           onChange={handleChange}
           value={form.password}
           required
-         
         />
         <br />
         <input
@@ -61,7 +60,6 @@ export default function ResetPassword() {
           placeholder="Confirm Password"
           onChange={handleChange}
           value={form.newpassword}
-         
         />
         <br />
         <button
@@ -72,5 +70,5 @@ export default function ResetPassword() {
         </button>
       </form>
     </div>
-  )
+  );
 }
