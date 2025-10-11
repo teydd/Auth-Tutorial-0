50f8ff6bac24654e30f485627a835010c9ccdfdf
teydd <chemosted@gmail.com>
2025-10-11 23:33:20 +0300

    Prettier

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import { useAuthStore } from '../store/authStore'
-import { Navigate } from 'react-router-dom'
+import React from "react";
+import { useAuthStore } from "../store/authStore";
+import { Navigate } from "react-router-dom";
 
-export const ProtectedRoute = ({children}) => {
-    const {isAuthenticated,isCheckingAuth} = useAuthStore()
-    if (isCheckingAuth) {
-    return
+export const ProtectedRoute = ({ children }) => {
+  const { isAuthenticated, isCheckingAuth } = useAuthStore();
+  if (isCheckingAuth) {
+    return;
   }
-  return isAuthenticated ? children :<Navigate to={"/signin"}></Navigate>
-}
+  return isAuthenticated ? children : <Navigate to={"/signin"}></Navigate>;
+};
