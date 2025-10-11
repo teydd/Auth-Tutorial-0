6329cf645aea2abcf4fc72cb2427d7cf41611f72
teydd <chemosted@gmail.com>
2025-10-11 23:33:00 +0300

    Prettier

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,31 +10,66 @@ import Signin from "./pages/Signin";
 import Verify from "./pages/Verify";
 import ForgetPassword from "./pages/ForgetPassword";
 import ResetPassword from "./pages/ResetPassword";
-import {Toaster} from "react-hot-toast"
+import { Toaster } from "react-hot-toast";
 import { useAuthStore } from "./store/authStore";
 import { RedirectingRoute } from "./components/RedirectingRoute";
 import { ProtectedRoute } from "./components/ProtectedRoute";
 
 function App() {
-  const {isCheckingAuth,checkAuth,isAuthenticated,user} = useAuthStore()
-  useEffect(()=>{
-    checkAuth()
-  },[checkAuth])
+  const { isCheckingAuth, checkAuth, isAuthenticated, user } = useAuthStore();
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
 
-  console.log("User",user)
-  console.log("is Authenticated",isAuthenticated)
+  console.log("User", user);
+  console.log("is Authenticated", isAuthenticated);
   return (
     <>
       <Router>
         <Nav></Nav>
         <Routes>
           <Route path="/" element={<Home></Home>}></Route>
-          <Route path="/dashboard" element={<ProtectedRoute><Dashboard></Dashboard></ProtectedRoute>}></Route>
-          <Route path="/signup" element={<RedirectingRoute><Signup></Signup></RedirectingRoute>}></Route>
-          <Route path="/signin" element={<RedirectingRoute><Signin></Signin></RedirectingRoute>}></Route>
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard></Dashboard>
+              </ProtectedRoute>
+            }
+          ></Route>
+          <Route
+            path="/signup"
+            element={
+              <RedirectingRoute>
+                <Signup></Signup>
+              </RedirectingRoute>
+            }
+          ></Route>
+          <Route
+            path="/signin"
+            element={
+              <RedirectingRoute>
+                <Signin></Signin>
+              </RedirectingRoute>
+            }
+          ></Route>
           <Route path="/verify" element={<Verify></Verify>}></Route>
-          <Route path="/forgot-password" element={<RedirectingRoute><ForgetPassword></ForgetPassword></RedirectingRoute>}></Route>
-          <Route path="/reset-password/:token" element={<RedirectingRoute><ResetPassword></ResetPassword></RedirectingRoute>}></Route>
+          <Route
+            path="/forgot-password"
+            element={
+              <RedirectingRoute>
+                <ForgetPassword></ForgetPassword>
+              </RedirectingRoute>
+            }
+          ></Route>
+          <Route
+            path="/reset-password/:token"
+            element={
+              <RedirectingRoute>
+                <ResetPassword></ResetPassword>
+              </RedirectingRoute>
+            }
+          ></Route>
         </Routes>
         <Toaster></Toaster>
       </Router>
