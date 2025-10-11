836da5b2066794839f716e592a3334f24d080aeb
teydd <chemosted@gmail.com>
2025-10-11 23:33:36 +0300

    Prettier

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,37 +1,41 @@
 import React from "react";
 import ProfileCard from "../bits/ProfileCard";
-import teddy from "../assets/teddy.jpg"
+import teddy from "../assets/teddy.jpg";
 import { useAuthStore } from "../store/authStore";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 export default function Dashboard() {
-  const {user,logout} = useAuthStore()
-  const navigate = useNavigate()
+  const { user, logout } = useAuthStore();
+  const navigate = useNavigate();
 
-  const handleLogOut = async()=>{
-    await logout()
-    navigate("/signin")
-    toast.success("Logout successful")
-
-  }
+  const handleLogOut = async () => {
+    await logout();
+    navigate("/signin");
+    toast.success("Logout successful");
+  };
   return (
     <>
       <p className="text-center lead py-2">Welcome {user?.name}</p>
-      <button onClick={handleLogOut} className="float-end btn btn-danger">Logout</button>
+      <button onClick={handleLogOut} className="float-end btn btn-danger">
+        Logout
+      </button>
       <br />
-      <div className="align-items-center justify-content-center d-flex" style={{height:"70vh"}}>
+      <div
+        className="align-items-center justify-content-center d-flex"
+        style={{ height: "70vh" }}
+      >
         <ProfileCard
-  name="Teddy Chemos"
-  title="Software Engineer"
-  handle="Teddy"
-  status="Online"
-  contactText="Contact Me"
-  avatarUrl={teddy}
-  showUserInfo={true}
-  enableTilt={true}
-  enableMobileTilt={true}
-  onContactClick={() => console.log('Contact clicked')}
-/>
+          name="Teddy Chemos"
+          title="Software Engineer"
+          handle="Teddy"
+          status="Online"
+          contactText="Contact Me"
+          avatarUrl={teddy}
+          showUserInfo={true}
+          enableTilt={true}
+          enableMobileTilt={true}
+          onContactClick={() => console.log("Contact clicked")}
+        />
       </div>
     </>
   );
