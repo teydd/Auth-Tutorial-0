25b89796a0f250cee90b80e4a75c78b0dc593914
teydd <chemosted@gmail.com>
2025-10-11 23:34:00 +0300

    Prettier

--- server/index.js
+++ server/index.js
@@ -1,34 +1,39 @@
-const express = require ("express")
-const mongoose = require("mongoose")
-const cookieParser = require("cookie-parser")
-const cors = require("cors")
-require("dotenv").config()
-const auth = require("./routes/authRoute")
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+require("dotenv").config();
+const auth = require("./routes/authRoute");
 
-const helmet = require("helmet")
-const ratelimit = require("express-rate-limit")
+const helmet = require("helmet");
+const ratelimit = require("express-rate-limit");
 
-PORT = process.env.PORT
+PORT = process.env.PORT;
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cookieParser())
-app.use(cors({origin:process.env.CLIENT_ORIGIN,credentials:true}))
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ origin: process.env.CLIENT_ORIGIN, credentials: true }));
 
-app.use(helmet())
-app.use(ratelimit({
-    windowMS:10*60*1000,
-    max:50
-}))
+app.use(helmet());
+app.use(
+  ratelimit({
+    windowMS: 10 * 60 * 1000,
+    max: 50,
+  })
+);
 
-app.use("/",auth)
+app.use("/", auth);
 
-mongoose.connect(process.env.Test).then(()=>{
-    console.log("DB connected succesfully")
-    app.listen(PORT,()=>{
-        console.log("Server running in port", PORT)
-    })
-}).catch(()=>{
-    console.log("Error connecting to DB")
-})
+mongoose
+  .connect(process.env.Test)
+  .then(() => {
+    console.log("DB connected succesfully");
+    app.listen(PORT, () => {
+      console.log("Server running in port", PORT);
+    });
+  })
+  .catch(() => {
+    console.log("Error connecting to DB");
+  });
